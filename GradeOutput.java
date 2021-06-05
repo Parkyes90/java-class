637561ec7ef8cb6430b587d71a507dd3520de9bc
parkyes90 <parkyes90@gmail.com>
2021-06-05 12:41:23 +0900

    Apply google code style

--- GradeOutput.java
+++ GradeOutput.java
@@ -1,24 +1,25 @@
 class Grade {
-    int e;
-    int m;
 
-    void output_grade() {
-        System.out.println(e + m);
-    }
+  int e;
+  int m;
+
+  void output_grade() {
+    System.out.println(e + m);
+  }
 }
 
-
 public class GradeOutput {
-    public static void main(String args[]) {
-        Grade g1, g2;
-        g1 = new Grade();
-        g2 = new Grade();
-        g1.e = 90;
-        g1.m = 85;
-        g2.e = 80;
-        g2.m = 80;
 
-        g1.output_grade();
-        g2.output_grade();
-    }
+  public static void main(String args[]) {
+    Grade g1, g2;
+    g1 = new Grade();
+    g2 = new Grade();
+    g1.e = 90;
+    g1.m = 85;
+    g2.e = 80;
+    g2.m = 80;
+
+    g1.output_grade();
+    g2.output_grade();
+  }
 }
