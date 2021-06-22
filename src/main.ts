0a4545b144757a79712ebd6d5043f0121008300f
parkyes90 <parkyes90@gmail.com>
2021-06-22 21:54:17 +0900

    build: initialize

--- src/main.ts
+++ src/main.ts
@@ -5,4 +5,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
-bootstrap();
+bootstrap()
+  .then(() => {
+    console.log('Server Started!');
+  })
+  .catch((e) => {
+    console.log(e);
+  });
