fdbfe14f4423a7f6b70680e1375264d58953c812
parkyes90 <parkyes90@gmail.com>
2021-06-25 23:24:00 +0900

    test: movie service test

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { NotFoundException } from '@nestjs/common';
 
 describe('MoviesService', () => {
   let service: MoviesService;
@@ -14,5 +15,33 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('should be return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return a movie', () => {
+      const createdMovie = service.create({
+        title: 'Test M',
+        genres: ['test'],
+        year: 2000,
+      });
+      const movie = service.getOne(createdMovie.id);
+      expect(movie).toBeDefined();
+      expect(movie.id).toEqual(createdMovie.id);
+    });
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(1234);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Movie with Id');
+      }
+    });
   });
 });
