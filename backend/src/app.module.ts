6d57d8b0350be924e342f7c534e86283782c30ef
parkyes90 <parkyes90@gmail.com>
2021-06-29 22:23:15 +0900

    build: setup typeorm & connect postgres

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
 
 @Module({
   imports: [
+    RestaurantsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.PG_HOST,
+      port: parseInt(process.env.PG_PORT),
+      username: process.env.PG_USER,
+      database: process.env.PG_DATABASE,
+      password: process.env.PG_PASSWORD,
+      logging: true,
+      synchronize: true,
+    }),
     GraphQLModule.forRoot({
       autoSchemaFile: true,
     }),
-    RestaurantsModule,
   ],
   controllers: [],
   providers: [],
