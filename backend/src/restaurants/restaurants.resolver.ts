b3e3963285c217e86558b35b908038f9391eaf69
parkyes90 <parkyes90@gmail.com>
2021-06-30 22:43:57 +0900

    feat: Connect entity with service

--- backend/src/restaurants/restaurants.resolver.ts
+++ backend/src/restaurants/restaurants.resolver.ts
@@ -1,12 +1,14 @@
 import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurants.entity';
 import { CreateRestaurantDto } from './dtos/create-restaurant.dto';
+import { RestaurantsService } from './restaurants.service';
 
 @Resolver(() => Restaurant)
 export class RestaurantsResolver {
+  constructor(private readonly restaurantService: RestaurantsService) {}
   @Query(() => [Restaurant])
-  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
-    return [];
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantService.getAll();
   }
   @Mutation(() => Boolean)
   createRestaurant(@Args() createRestaurantDto: CreateRestaurantDto): boolean {
