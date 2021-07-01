a223ff0d2bf6485ec749fa84d373d20dbaaba046
parkyes90 <parkyes90@gmail.com>
2021-07-01 22:24:10 +0900

    feat: Add validation

--- backend/src/restaurants/entities/restaurants.entity.ts
+++ backend/src/restaurants/entities/restaurants.entity.ts
@@ -1,5 +1,6 @@
 import { Field, InputType, ObjectType } from '@nestjs/graphql';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 @InputType({ isAbstract: true })
 @ObjectType()
@@ -11,10 +12,13 @@ export class Restaurant {
 
   @Field(() => String)
   @Column()
+  @IsString()
+  @Length(5)
   name: string;
 
   @Field(() => Boolean, { nullable: true })
   @Column()
+  @IsBoolean()
   isVegan: boolean;
 
   @Field(() => String)
