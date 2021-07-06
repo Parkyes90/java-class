2e47a5ebc62df451214c470ab028fd26f43cb1fb
parkyes90 <parkyes90@gmail.com>
2021-07-06 21:34:14 +0900

    feat: add exception

--- backend/src/tokens/tokens.middleware.ts
+++ backend/src/tokens/tokens.middleware.ts
@@ -1,4 +1,4 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { ForbiddenException, Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 import { TokensService } from './tokens.service';
 import { UsersService } from '../users/users.service';
@@ -13,11 +13,14 @@ export class TokensMiddleware implements NestMiddleware {
   async use(req: Request, res: Response, next: NextFunction) {
     const token = req.headers['x-token'];
     if (token) {
-      const decoded = this.tokensService.verify(token.toString()) as JwtPayload;
       try {
+        const decoded = this.tokensService.verify(
+          token.toString(),
+        ) as JwtPayload;
+        console.log(decoded);
         req['user'] = await this.usersService.findById(decoded.id);
       } catch (e) {
-        throw e;
+        throw new ForbiddenException(e);
       }
     }
     next();
