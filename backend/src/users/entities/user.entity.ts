301fd4e3679d9737491c464e1cc250fabb48b860
parkyes90 <parkyes90@gmail.com>
2021-07-08 23:06:57 +0900

    feat: ignore password save

--- backend/src/users/entities/user.entity.ts
+++ backend/src/users/entities/user.entity.ts
@@ -27,7 +27,7 @@ export class User extends CoreEntity {
   @IsEmail()
   email: string;
 
-  @Column()
+  @Column({ select: false })
   @Field(() => String)
   password: string;
 
@@ -48,7 +48,9 @@ export class User extends CoreEntity {
   @BeforeUpdate()
   async hashPassword(): Promise<void> {
     try {
-      this.password = await bcrypt.hash(this.password, 10);
+      if (this.password) {
+        this.password = await bcrypt.hash(this.password, 10);
+      }
     } catch (e) {
       throw new InternalServerErrorException(e);
     }
