388388cc7744a634a814956bd3fe16c90817c024
parkyes90 <parkyes90@gmail.com>
2021-07-19 23:40:27 +0900

    test: add mocking

--- backend/src/users/users.service.spec.ts
+++ backend/src/users/users.service.spec.ts
@@ -1,11 +1,48 @@
 import { Test } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Verification } from './entities/verification.entity';
+import { TokensService } from '../tokens/tokens.service';
+import { MailsService } from '../mails/mails.service';
+
+const mockRepository = {
+  findOne: jest.fn(),
+  save: jest.fn(),
+  create: jest.fn(),
+};
+
+const mockTokensService = {
+  sign: jest.fn(),
+  verify: jest.fn(),
+};
+const mockMailsService = {
+  sendVerificationEmail: jest.fn(),
+};
 
 describe('UsersService', () => {
   let service: UsersService;
   beforeAll(async () => {
     const module = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockRepository,
+        },
+        {
+          provide: getRepositoryToken(Verification),
+          useValue: mockRepository,
+        },
+        {
+          provide: TokensService,
+          useValue: mockTokensService,
+        },
+        {
+          provide: MailsService,
+          useValue: mockMailsService,
+        },
+      ],
     }).compile();
     service = module.get<UsersService>(UsersService);
   });
